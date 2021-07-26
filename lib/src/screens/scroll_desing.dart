02d3e90921eaa404a73b2022816708fae1f912c1
Sebastian Otalora <sebastianotalora@tyba.com>
2021-07-26 18:52:14 -0500

    Enhance the desing of second screen

--- lib/src/screens/scroll_desing.dart
+++ lib/src/screens/scroll_desing.dart
@@ -3,10 +3,48 @@ import 'package:flutter/material.dart';
 class ScrollScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [BackgroundScroll(), MainContainer()],
+    final boxDecoration = BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [
+          0.5,
+          0.5,
+        ],
+        colors: [
+          Color(0xff5EE8C5),
+          Color(0xff30BAD6),
+        ],
       ),
+    );
+    return Scaffold(
+      body: Container(
+        decoration: boxDecoration,
+        child: PageView(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: [
+            Page1(),
+            Page2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackgroundScroll(),
+        MainContainer(),
+      ],
     );
   }
 }
@@ -19,7 +57,6 @@ class BackgroundScroll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xff30BAD6),
       height: double.infinity,
       alignment: Alignment.topCenter,
       child: Image.asset('assets/img/scroll-1.png'),
@@ -64,6 +101,33 @@ class MainContainer extends StatelessWidget {
             color: Colors.white,
           )
         ],
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: Text(
+            'Bienvenido',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          style: TextButton.styleFrom(
+            backgroundColor: Color(0xFF308CC9),
+            shape: StadiumBorder(),
+          ),
+        ),
       ),
     );
   }
