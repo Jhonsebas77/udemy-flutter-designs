c838a1ec1bd56f6a95279e42f35fca65a57f8965
Sebastian Otalora <sebastianotalora@tyba.com>
2021-07-26 17:57:19 -0500

    Enhance the UI

--- lib/src/screens/basic_design.dart
+++ lib/src/screens/basic_design.dart
@@ -4,12 +4,60 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage(
-            'assets/img/landscape.jpeg',
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage(
+              'assets/img/landscape.jpeg',
+            ),
           ),
-        ),
+          Title()
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 10,
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'New York Avenue',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'New York',
+                style: TextStyle(
+                  color: Colors.black45,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(
+            Icons.star_rate,
+            color: Colors.red,
+          ),
+          Text('41'),
+        ],
       ),
     );
   }
