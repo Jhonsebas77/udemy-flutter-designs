d6aed11b3e8b0260352c7f5a88d0dc47d4903804
Sebastian Otalora <sebastianotalora@tyba.com>
2021-07-26 17:47:29 -0500

    Create a basic screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_designs/src/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'basic_design',
+      routes: {
+        'basic_design': (_) => BasicDesignScreen(),
+      },
     );
   }
 }
---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_designs/src/screens/basic_design.dart';
